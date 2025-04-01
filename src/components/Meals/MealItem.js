a168e47abcb66b15c85bf3c72b6096df6daac2bc
kaareltiitt6nts <kaarel-tiit.tonts@voco.ee>
2025-04-01 09:37:05 +0300

    menuitem ja edasine button stiilimine

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -5,15 +5,17 @@ const MealItem = (props) => {
     const {image, name, price, description} = props
 
     return (
-        <li>
-            <article>
+        <li className="product">
+            <article className="product-content">
                 <img src={require(`../../assets/${image}`)} alt={name} className="product-thumbnail"/>
                 <div>
-                    <h3>{name}</h3>
-                    <p>{Intl.NumberFormat("en-ee", {style:"currency", currency:"EUR"}).format(price)}</p>
-                    <p>{description}</p>
+                    <h3 className="product-name">{name}</h3>
+                    <p className="product-price">{Intl.NumberFormat("en-ee", {style:"currency", currency:"EUR"}).format(price)}</p>
+                    <p className="product-description">{description}</p>
                 </div>
-                <Button onClick={() => console.log("hi")}>Place an order</Button>
+                <div>
+                    <Button onClick={() => console.log("hi")}>Place an order</Button>
+                </div>
             </article>
         </li>
     )
